a7475db0733f6cc62c5dea20c098faa4c92206c4
elcolie <sarit@elcolie.com>
2019-04-24 21:49:04 +0700

    Be able to mark center of the detected text
    Signed-off-by: Sarit Ritwirune <sarit@elcolie.com>

--- playground.py
+++ playground.py
@@ -1,6 +1,7 @@
 import cv2
 
-from utils import read_points, four_point_transform
+from fours_point import read_points, four_point_transform
+from utils import get_center
 
 DEMO_DIR = 'demo/'
 
@@ -27,6 +28,20 @@ def tear_down_page(image_filename: str, coords_filename: str, dir: str):
         idx += 1
 
 
+def mark_center(image_filename: str, coords_filename: str):
+    """
+
+    :return:
+    """
+    im = cv2.imread(DEMO_DIR + image_filename)
+    pts = read_points(coords_filename)
+    center_pts = get_center(pts)
+    for pt in center_pts:
+        point = int(pt[0]), int(pt[1])
+        cv2.circle(im, point, 10, (0, 0, 255), -1)
+    cv2.imwrite(f'mark-center.png', im)
+
+
 if __name__ == '__main__':
-    tear_down_page('p23-181.png', 'res/p23-181.txt', 'cropped_sentences')
-    # convert_object(cv2.imread('perspective_transform/Original.png'), isDebug=True)
+    # tear_down_page('p23-181.png', 'res/p23-181.txt', 'cropped_sentences')
+    mark_center('p23-181.png', 'res/p23-181.txt')
